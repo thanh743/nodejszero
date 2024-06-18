52469f327a1eb16094bced13bbbfc3a69a2816b9
Mac <mac@Macs-MacBook-Pro.local>
2024-06-18 10:07:53 +0700

    18-jun

--- server.js
+++ server.js
@@ -5,10 +5,21 @@ const port = process.env.PORT || 8888 ;
 const hostname = process.env.HOST_NAME ;
 const configViewEngine = require('./src/config/viewEngine')
 const webRouter = require('./src/routers/web')
-
+const connection = require('./src/config/database');
+const { table } = require('console');
 configViewEngine(app)
 app.use('/', webRouter)
 
+// test connection to server
+
+connection.query(
+    // 'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45 ',
+    'SELECT * FROM  `User` u ',
+    function(err, result,fields) {
+        console.log("Result: ",result);
+        //console.log("fields: ",fields);
+    }
+);
 
 app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
